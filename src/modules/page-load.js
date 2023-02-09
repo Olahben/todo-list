9c0c142d4286c09add91d3f1ce082feb2177a57e
Ola <ohagenbendixen@gmail.com>
2023-02-09 07:09:07 +0100

    Create a sidebar and a toDo workspace

--- src/modules/page-load.js
+++ src/modules/page-load.js
@@ -4,7 +4,10 @@ const pageLoad = () => {
   const sidebar = document.createElement('div');
   sidebar.setAttribute('id', 'sidebar');
 
-  content.append(sidebar);
+  const toDoContent = document.createElement('div');
+  toDoContent.setAttribute('id', 'workspace');
+
+  content.append(sidebar, toDoContent);
 };
 
 export default pageLoad;
