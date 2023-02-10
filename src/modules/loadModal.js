673e022f43388fbcaac0d65f56f63b77b2f114e5
Ola <ohagenbendixen@gmail.com>
2023-02-10 07:08:23 +0100

    Wrap each form control section in a div

--- src/modules/loadModal.js
+++ src/modules/loadModal.js
@@ -7,20 +7,30 @@ const loadModal = () => {
 
   const modalForm = document.createElement('form');
   modalForm.innerHTML = `
+  <div class="title form-element">
   <label for="title">Title</label><br>
   <input type="text" name="title" id="title"><br>
+</div>
 
+<div class="descr form-element">
   <label for="descr">Description</label><br>
   <input type="text" name="descr" id="descr"><br>
+</div>
 
+<div class="due-date form-element">
   <label for="dueDate">Due Date</label><br>
   <input type="date" name="dueDate" id="dueDate"><br>
+</div>
 
+<div class="priority form-element">
   <label for="priority">priority</label><br>
   <input type="text" name="priority" id="priority"><br>
+</div>
 
+<div class="project form-element">
   <label for="project">Which project do you want to add this task to?</label><br>
-  <input type="text" name="project" id="project"><br>`;
+  <input type="text" name="project" id="project"><br>
+</div>`;
 
   modal.append(modalForm);
   workspace.append(modal);
