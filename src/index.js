35c89bc323a7904e2dc2be42945e83c393b86766
Ola <ohagenbendixen@gmail.com>
2023-02-12 09:31:42 +0100

    Make the logic that opens the modal on btn click

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import pageLoad from './modules/page-load';
 import './style.css';
+import loadModal from './modules/loadModal';
 
-document.addEventListener('DOMContentLoaded', pageLoad);
+pageLoad();
+document.querySelector('.add-to-do').addEventListener('click', () => {
+  loadModal();
+  const modal = document.querySelector('.modal');
+  modal.style.display = 'block';
+});
