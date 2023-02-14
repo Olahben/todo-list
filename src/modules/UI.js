93c057130cd4b6b4508bb21f06d176a5eb7b580a
Ola <ohagenbendixen@gmail.com>
2023-02-14 18:35:38 +0100

    Create a function that creates a project and appends it to the sidebar

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -2,4 +2,20 @@ const UI = (() => {
   const module = {};
 
   module.createToDo = () => {};
+
+  module.createProject = (title) => {
+    const sidebar = document.querySelector('#sidebar');
+    const project = document.createElement('div');
+    project.classList.add(`${title}`);
+    project.classList.add('project');
+    project.innerHTML = `
+    ${title}
+    <i class='bx bx-right-arrow-alt'></i>`;
+
+    sidebar.append(project);
+  };
+
+  return { module };
 })();
+
+export default UI;
